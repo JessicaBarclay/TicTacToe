50dbe22660da256bec3efc3e878ffc1a4b2c8d83
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-06-14 12:34:06 +0100

    Add winningCombinations to TicTacToe

--- spec/TicTacToeSpec.js
+++ spec/TicTacToeSpec.js
@@ -11,4 +11,11 @@ describe('TicTacToe', function(){
                                     {4 : []}, {5 : []}, {6 : []},
                                     {7 : []}, {8 : []}, {9 : []}]);
   });
+
+  it('has a property of winning combinations', function(){
+    expect(tictactoe.winningCombinations).toEqual([[1, 2, 3], [4, 5, 6],
+                                                   [7, 8, 9], [1, 5, 9],
+                                                   [3, 5, 7], [1, 4, 7],
+                                                   [2, 5, 8], [3, 6, 9]]);
+  });
 });
